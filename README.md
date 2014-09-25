7d98788bbab3a4dd81717bd65d53702bc039d6e3
Sean Walsh <sean@swalsh.org>
2014-09-25 16:55:23 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Imgur Scraper
+
+## Usage
+`node index.js -g [gallery url] -d [download directory]`
+
+* Gallery URL is required and must be the full path to an Imgur gallery.
+* Download directory is not required and will default to "download". Unless an absolute path is specified, the download directory will be created relative to `index.js`.
+
+## Thanks!
+Wouldn't have been possible without these modules:
+
+* https://github.com/inikulin/ineed
+* https://github.com/mikeal/request
+* https://github.com/substack/node-mkdirp
+* https://github.com/substack/minimist
---
+++ index.js
@@ -0,0 +1,58 @@
+var fs = require("fs"),
+	path = require("path"),
+	argv = require("minimist")( process.argv.slice( 2 ) ),
+	request = require("request"),
+	mkdirp = require("mkdirp"),
+	ineed = require("ineed");
+
+var gallery = argv.g,
+	dir = path.resolve( argv.d || "downloads" );
+
+var download = function( uri, filename, callback ) {
+	request( uri ).pipe( fs.createWriteStream( filename ) ).on( "close", callback );
+};
+
+var grabImages = function( hrefs ) {
+
+	mkdirp.sync( dir );
+
+	console.log( "Downloading " + hrefs.length + " images to " + dir + "..." );
+
+	hrefs.forEach( function( href ) {
+
+		var filename = href.split("/").slice(-1)[0];
+
+		download( href, dir + "/" + filename, function() {
+			console.log( "Downloaded " + filename );
+		});
+
+	});
+
+};
+
+if( gallery && dir ) {
+
+	ineed.collect.hyperlinks.from( gallery, function( err, response, result ) {
+
+		var hrefs = [];
+
+		result.hyperlinks.forEach( function( link ) {
+
+			// there must be a better way to find the correct links..
+			if( link.text && link.text == "View full resolution" ) {
+				hrefs.push( link.href );
+			}
+			
+		});
+
+		grabImages( hrefs );
+
+	});
+
+} else {
+
+	console.log( "Usage: node index.js -g [gallery url] -d [download directory]" );
+	console.log( "\t-g The full URL to an Imgur gallery" );
+	console.log( "\t-d The path to the download directory. Default is \"download\".");
+	
+}
